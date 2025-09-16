f1cc5fafed53eec14b275d03c090faa28d857cda
jinsu <biosvos@gmail.com>
2025-09-16 08:21:46 +0000

    add new command line

--- cmd/project/main.go
+++ cmd/project/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/neatflowcv/project/internal/app/flow"
+	"github.com/urfave/cli/v3"
+)
 
 func main() {
-	log.Println("Hello, World!")
+	var project string
+
+	service := flow.NewService()
+
+	cmd := cli.Command{ //nolint:exhaustruct
+		Commands: []*cli.Command{
+			{
+				Name:    "new",
+				Usage:   "new project",
+				Aliases: []string{"n"},
+				Action: func(ctx context.Context, c *cli.Command) error {
+					if project == "" {
+						return cli.Exit("project name is required", 1)
+					}
+
+					projectName := extractProjectName(project)
+					moduleName := extractModuleName(project)
+					log.Println("create project", projectName, moduleName)
+					err := service.NewProject(projectName, moduleName)
+					if err != nil {
+						return cli.Exit(err.Error(), 1)
+					}
+
+					return nil
+				},
+				Arguments: []cli.Argument{
+					&cli.StringArg{ //nolint:exhaustruct
+						Name:        "name",
+						Destination: &project,
+						UsageText:   "https://github.com/neatflowcv/project or https://github.com/neatflowcv/project.git or project",
+					},
+				},
+			},
+		},
+	}
+
+	err := cmd.Run(context.Background(), os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func extractModuleName(projectName string) string {
+	ret := strings.TrimSuffix(projectName, ".git")
+	ret = strings.TrimPrefix(ret, "https://")
+	ret = strings.TrimPrefix(ret, "http://")
+
+	return ret
+}
+
+func extractProjectName(project string) string {
+	project = strings.TrimSuffix(project, ".git")
+	parts := strings.Split(project, "/")
+	ret := parts[len(parts)-1]
+
+	return ret
 }
--- internal/app/flow/service.go
+++ internal/app/flow/service.go
@@ -5,3 +5,7 @@ type Service struct{}
 func NewService() *Service {
 	return &Service{}
 }
+
+func (s *Service) NewProject(projectName string, moduleName string) error {
+	return nil
+}
